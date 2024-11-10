5a1be986e08d2ce5e034c9f613f209d253a8f52d
Pavel Vorobyev <p.vorobyev@inbox.ru>
2024-11-10 22:06:33 +0300

    integration test for checkSendAbility service

---
+++ src/test/kotlin/dev/voroby/telegram/gateway/IntegrationTests.kt
@@ -0,0 +1,30 @@
+package dev.voroby.telegram.gateway
+
+import dev.voroby.telegram.gateway.common.domain.Response
+import dev.voroby.telegram.gateway.common.infrastructure.Protocol
+import dev.voroby.telegram.gateway.service.checkSendAbility.CheckSendAbility
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+
+class IntegrationTests : FunSpec({
+
+    val protocol = Protocol.createHttpProtocol()
+
+    val telegramGateway = TelegramGateway.create(
+        accessToken = "your_token",
+        protocol = protocol
+    )
+
+    fun onProtocolSuccess(response: Response) = println(response.toString())
+
+    afterSpec {
+        telegramGateway.close()
+    }
+
+    test("Invoke check ability to send the code to the user") {
+        val request = CheckSendAbility.Request("phone_number_in_international_format")
+        val checkSendAbility = telegramGateway.checkSendAbility(request)
+        checkSendAbility.isRight().shouldBeTrue()
+        checkSendAbility.onRight { onProtocolSuccess(it) }
+    }
+})
